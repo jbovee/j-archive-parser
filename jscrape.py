ccb92d369db0fc46b8f0e7d2fda94b30fc1da6ca
jbovee <Mitouch.b@gmail.com>
2017-04-07 16:04:38 -0400

    Divided parts into separate functions. Also condensed method of obtaining season names/number and episode ids

--- jscrape.py
+++ jscrape.py
@@ -3,37 +3,53 @@ import requests
 import re
 
 ### Maybe look into breaking up into functions ###
+# Break up CSVs into seasons
 
-seasons = requests.get('http://j-archive.com/listseasons.php')
+SECONDS_BETWEEN_REQUESTS = 5
 
-soupSeasons = BeautifulSoup(seasons.text, 'html.parser')
+def main():
+	#Open/create file to be written to
+	seasons = get_all_seasons()
 
-r = re.compile(r'showseason\.php\?season=')
+def get_all_seasons():
+	seasonPage = requests.get('http://j-archive.com/listseasons.php')
+	soupSeasons = BeautifulSoup(seasonPage.text, 'html.parser')
+	r = re.compile(r'season=[0-9a-zA-Z]+')
+	r2 = re.compile(r'showseason\.php\?season=')
 
-seasonLinks = []
+	seasons = [r.search(link).group(0).split('=')[1] for link in soupSeasons.find_all('a') if r2.match(link.get('href'))]
 
-for link in soupSeasons.find_all('a'):
-	reg = r.match(link.get('href'))
-	if reg:
-		seasonLinks.append("http://j-archive.com/"+link.get('href'))
+	for season in seasons[::-1][-1:]:
+		get_episodes(season)
 
-r = re.compile(r'http:\/\/www\.j-archive\.com\/showgame\.php\?game_id=')
-
-for season in seasonLinks[::-1][-1:]:
+def parse_season(season):
 	episodes = requests.get(season)
 	soupEpisodes = BeautifulSoup(episodes.text, 'html.parser')
+	r = re.compile(r'game_id=[0-9]+')
+	r2 = re.compile(r'http:\/\/www\.j-archive\.com\/showgame\.php\?game_id=[0-9]+')
 
-	episodeLinks = []
+	episodeIds = [r.search(link.get('href')).group(0).split('=')[1] for link in soupEpisodes.find_all('a') if r2.match(link.get('href'))]
 
-	for link in soupEpisodes.find_all('a'):
-		reg = r.match(link.get('href'))
-		if reg:
-			episodeLinks.append(link.get('href'))
+	#write csv titles
+	for id in episodeIds:
+		ep = parse_episode(id)
+		write_episode(ep,outfile)
 
-	for episodeLink in episodeLinks:
-		episode = requests.get(episodeLink)
-		soupEpisode = BeautifulSoup(episode.text, 'html.parser')
+def parse_episode(episodeLink):
+	episode = requests.get(episodeLink)
+	soupEpisode = BeautifulSoup(episode.text, 'html.parser')
 
-		#print(soupEpisode.find(id='game_title'))
+	#print(soupEpisode.find(id='game_title'))
 
-		isBlank = True if soupEpisode.find(id='jeopardy_round') is None else False
+	isBlank = True if soupEpisode.find(id='jeopardy_round') is None else False
+
+def parse_round():
+	if round:
+		#Jeopardy and double jeopardy rounds
+	else:
+		#Final jeopardy
+
+def write_episode(episode, outfile):
+
+if __name__ == "__main__":
+	main()
